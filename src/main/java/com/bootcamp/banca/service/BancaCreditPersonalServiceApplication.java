d7b136a4544dd56234425ef5c00a84a6a51cbfb0
Andre Robinson Yataco Lermo <ayatacol>
2021-08-17 14:41:49 -0500

    Se suben cambios

---
+++ src/main/java/com/bootcamp/banca/service/BancaCreditPersonalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.banca.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaCreditPersonalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancaCreditPersonalServiceApplication.class, args);
+	}
+
+}
